279c112f8e851c62d25d5145fc3bc52894dd30c7
davenonymous <david.hilberath@gmx.de>
2025-11-03 21:17:16 +0100

    Add mod_loaded condition to generated loot tables
    This should fix a lot of log spam. See
    https://github.com/davenonymous/BonsaiTrees/issues/379

--- src/main/perl/fix_registry_jsons.pl
+++ src/main/perl/fix_registry_jsons.pl
@@ -12,10 +12,12 @@ die "Usage: $0 <directory>\n" unless $dir;
 die "Directory $dir does not exist\n" unless -d $dir;
 
 my $jsx = JSON::XS->new->utf8->pretty->canonical;
-my $json_files = Mojo::File->new($dir)->list_tree->grep(sub { $_->dirname->basename eq 'soiltype' && $_->basename =~ /\.json$/ });
+my $json_files = Mojo::File->new($dir)->list_tree->grep(sub { ($_->dirname =~ m/loot_table/ || $_->dirname->basename eq 'soiltype') && $_->basename =~ /\.json$/ });
 
 foreach my $file ($json_files->each) {
     my $modId = $file->dirname->dirname->dirname->dirname->dirname->basename;
+    $modId = $file->dirname->basename if $modId eq 'data';
+
     my $content = $file->slurp('UTF-8');
     my $data;
     try {
